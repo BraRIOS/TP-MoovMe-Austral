f8a0adbb86604a0ab3306ad2e6097183b6528ca4
juaan30 <juanmartingio@hotmail.com>
2019-10-19 21:26:04 -0300

    Trip

--- Tariff.java
+++ Tariff.java
@@ -2,4 +2,16 @@ public class Tariff {
     double price;
     Zone zone;
 
+    public Tariff(double price, Zone zone) {
+        this.price = price;
+        this.zone = zone;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Zone getZone() {
+        return zone;
+    }
 }
