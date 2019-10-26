2b52c9f3cc14ae6b3129e2fab94e7ad72fca45fb
juaan30 <juanmartingio@hotmail.com>
2019-10-26 00:19:42 -0300

    Actualizacion trip,tariff e implementacion de Consumos

---
+++ Consumption.java
@@ -0,0 +1,11 @@
+public class Consumption{
+    private double consumptionPrice;
+
+    public Consumption(double consumptionPrice) {
+        this.consumptionPrice = consumptionPrice;
+    }
+
+    public double getConsumptionPrice() {
+        return consumptionPrice;
+    }
+}
